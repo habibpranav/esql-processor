ff8c145d918c4eb9ba01f6c9f215791b95967e81
pranavhabib <pranavhabib1@gmail.com>
2025-11-29 02:54:52 -0500

    Added Row and DatabaseFetcher

--- src/main/java/edu/stevens/cs562/Row.java
+++ src/main/java/edu/stevens/cs562/Row.java
@@ -1,3 +1,38 @@
 package edu.stevens.cs562;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Row {
+
+    private HashMap<String, Object> values;
+
+    public Row() {
+        this.values = new HashMap<>();
+    }
+
+    // Store a column value
+    public void put(String columnName, Object value) {
+        values.put(columnName, value);
+    }
+
+    // Retrieve a column value
+    public Object get(String columnName) {
+        return values.get(columnName);
+    }
+
+    // Retrieve all column names
+    public ArrayList<String> getColumnNames() {
+        return new ArrayList<>(values.keySet());
+    }
+
+    // Return internal HashMap directly
+    public HashMap<String, Object> getValues() {
+        return values;
+    }
+
+    @Override // For debugging
+    public String toString() {
+        return values.toString();
+    }
 }
