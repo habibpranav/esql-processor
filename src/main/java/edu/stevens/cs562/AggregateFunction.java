9c7e619241fa71db0b85b3f490df4e7b4b37b12d
pranavhabib <pranavhabib1@gmail.com>
2025-12-09 05:44:47 -0500

    Refactored AggregateFunction

--- src/main/java/edu/stevens/cs562/AggregateFunction.java
+++ src/main/java/edu/stevens/cs562/AggregateFunction.java
@@ -1,3 +1,30 @@
 package edu.stevens.cs562;
+
 public class AggregateFunction {
+    private String functionName;
+    private String groupingVarName;
+    private String attribute;
+
+    public AggregateFunction(String functionName, String groupingVarName, String attribute) {
+        this.functionName = functionName;
+        this.groupingVarName = groupingVarName;
+        this.attribute = attribute;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getGroupingVarName() {
+        return groupingVarName;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    @Override
+    public String toString() {
+        return functionName + "(" + groupingVarName + "." + attribute + ")";
+    }
 }
